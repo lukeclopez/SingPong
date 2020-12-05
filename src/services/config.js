309eebffd65c760ae0be573c795239299621e5c6
Luke Lopez <luke@aethonlabs.com>
2020-12-05 15:30:30 -0500

    Simplified settings system

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,27 @@
-export const getTimeLimit = () =>
-  parseInt(localStorage.getItem("timeLimit")) + 1 || 61;
-export const setTimeLimit = (s) => localStorage.setItem("timeLimit", s);
+const settingsKey = "settings";
 
-export const getWordSet = () => localStorage.getItem("wordSet") || "Set 1";
-export const setWordSet = (wordSet) => localStorage.setItem("wordSet", wordSet);
+const defaultSettings = {
+  timeLimit: 61,
+  wordSet: "Set 1",
+};
+
+export const setSetting = (key, value) => {
+  validateKey(key);
+  const settings = JSON.parse(localStorage.getItem(settingsKey));
+  settings[key] = value;
+  window.localStorage.setItem(settings, JSON.stringify(settings));
+};
+
+export const getSetting = (key) => {
+  validateKey(key);
+  const settings = JSON.parse(localStorage.getItem(settingsKey));
+  return settings[key] || defaultSettings[key];
+};
+
+const validKeys = Object.keys(defaultSettings);
+const validateKey = (key) => {
+  if (!validKeys.includes(key)) throw new Error("Invalid settings key");
+};
+
+if (!localStorage.getItem(settingsKey))
+  localStorage.setItem(settingsKey, JSON.stringify(defaultSettings));
