511066dafeb4f738d4eae254cb2688216a09e263
Luke Lopez <38708015+lukeclopez@users.noreply.github.com>
2026-01-04 11:17:16 +0300

    Split word sets into different files

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import theocratic from "./Theocratic.json";
+import theocraticSpanish from "./Theocratic Spanish.json";
+import set1 from "./Set 1.json";
+
+const words = {
+  "Theocratic": theocratic,
+  "Theocratic Spanish": theocraticSpanish,
+  "Set 1": set1,
+};
+
+export default words;
