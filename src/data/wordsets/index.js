931763fb1293d9e13b300003f397f2185bda8061
Luke Lopez <38708015+lukeclopez@users.noreply.github.com>
2026-01-04 11:29:55 +0300

    Rename word sets

--- src/data/wordsets/index.js
+++ src/data/wordsets/index.js
@@ -1,13 +1,13 @@
-import theocratic from "./Theocratic.json";
+import theocraticEnglish from "./Theocratic English.json";
 import theocraticSpanish from "./Theocratic Spanish.json";
 import theocraticSwahili from "./Theocratic Swahili.json";
-import set1 from "./Set 1.json";
+import generalEnglish from "./General English.json";
 
 const words = {
-  "Theocratic": theocratic,
+  "Theocratic English": theocraticEnglish,
   "Theocratic Spanish": theocraticSpanish,
   "Theocratic Swahili": theocraticSwahili,
-  "Set 1": set1,
+  "General English": generalEnglish,
 };
 
 export default words;
