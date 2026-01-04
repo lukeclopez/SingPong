48e56bedf99ebc6bc985c24d0f44eaad4e585969
Luke Lopez <38708015+lukeclopez@users.noreply.github.com>
2026-01-04 11:25:25 +0300

    Wire up new wordset

--- src/data/wordsets/index.js
+++ src/data/wordsets/index.js
@@ -1,10 +1,12 @@
 import theocratic from "./Theocratic.json";
 import theocraticSpanish from "./Theocratic Spanish.json";
+import theocraticSwahili from "./Theocratic Swahili.json";
 import set1 from "./Set 1.json";
 
 const words = {
   "Theocratic": theocratic,
   "Theocratic Spanish": theocraticSpanish,
+  "Theocratic Swahili": theocraticSwahili,
   "Set 1": set1,
 };
 
